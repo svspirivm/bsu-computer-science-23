19216bdb423f1e3d5408a322864008d1362e9cab
svspirivm <k.shymborskaya@gmail.com>
2021-02-28 22:43:14 +0300

    TA control1 task2 added

--- theory_of_algorithm/control1/task2.horse/Source.cpp
+++ theory_of_algorithm/control1/task2.horse/Source.cpp
@@ -1,41 +1,69 @@
 #include<iostream>
-#include<fstream>
+#pragma warning(disable : 4996)
+#include <stdio.h>
 #include<vector>
+#include<queue>
 
-class Point {
-public:
-	int  x = 0;
-	int y = 0;
-
-	Point(int x, int y) {
-		this->x = x;
-		this->y = y;
-	}
-
-	bool overflow(const int& n, const int& m, const std::vector<std::vector<int>>& field) {
-		if ((x <= 0) || (y <= 0) || (x > n) || (y > m) || (field[x][y] == -1)) return false;
-			field[x][y] = false;
-		return true;
-	}
-}
+//bool outOfBounds(const int& n, const int& m, const std::pair<int, int>& coordinates, const std::vector<std::vector<int>>& field) {
+	//if ((coordinates.first < 0) || (coordinates.second < 0) || 
+	//	(coordinates.first >= n) || (coordinates.second >= m) || 
+	//	(field[coordinates.first][coordinates.second] == -1)) return true;
+	//return false;
+//}
 
 int main() {
-	int n, m;
-	std::vector<std::vector<int>> cant_get;
-	std::fstream input("in.txt");
+	int n = 0, m = 0;
+	std::vector<std::vector<int>> field;
+	const char* filename = "in.txt";
+	std::ios_base::sync_with_stdio(false);
+	std::cin.tie(nullptr);
+	FILE* file = freopen(filename, "r", stdin);
+	fscanf(file, "%d", &n);
+	fscanf(file, "%d", &m);
 	for (size_t i = 0; i < n; i++) {
 		std::vector<int> tmp;
 		for (size_t j = 0; j < m; j++) {
 			int element = 0;
-			input >> element;
+			fscanf(file, "%d", &element);
 			tmp.push_back(element);
 		}
-		cant_get.push_back(tmp);
+		field.push_back(tmp);
 	}
 	int x1 = 0, y1 = 0, x2 = 0, y2 = 0;
-	input >> x1 >> y1 >> x2 >> y2;
-	Point begin(x1, y1);
-	Point end(x2, y2);
+	fscanf(file, "%d", &x1);
+	fscanf(file, "%d", &y1);
+	fscanf(file, "%d", &x2);
+	fscanf(file, "%d", &y2);
+	fclose(file);
+	std::pair<int, int> begin(x1 - 1, y1 - 1);
+	std::pair<int, int> end(x2 - 1, y2 - 1);
+	const std::vector<std::pair<int, int>> moves = { {2, 1}, {2, -1}, {1, 2}, {1, -2}, {-1, 2}, {-1, -2}, {-2, 1}, {-2, -1} };
+	std::queue<std::pair<int, int>> positions;
+	if (field[begin.first][begin.second] == 0) {
+		positions.push(std::make_pair(begin.first, begin.second));
+	}
 
+	while (!positions.empty()) {
+		std::pair<int, int> current_position = positions.front();
+		positions.pop();
+		if (current_position == end) break;
+		if (field[current_position.first][current_position.second] == -1) continue;
+		for (size_t j = 0; j < moves.size(); j++) {
+			std::pair<int, int> new_position(current_position.first + moves[j].first, current_position.second + moves[j].second);
+			if (!((new_position.first < 0) || (new_position.second < 0) ||
+				(new_position.first >= n) || (new_position.second >= m) ||
+				(field[new_position.first][new_position.second] == -1))) {
+
+				field[new_position.first][new_position.second] = 1 + field[current_position.first][current_position.second];
+				positions.push(new_position);
+			}
+		}
+		field[current_position.first][current_position.second] = -1;
+	}
+	const char* output = "out.txt";
+	FILE* file_output = freopen(output, "w", stdout);
+	if (field[end.first][end.second] == -1)fprintf(file_output, "%s", "No");
+	fprintf(file_output, "%d", field[end.first][end.second]);
+	fclose(file_output);
 	return 0;
 }
